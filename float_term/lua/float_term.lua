5cc492923c0063cf1b8717cdf2c464d22419396b
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-26 19:09:54 +1100

    wip: minor changes to inner workings

--- float_term/lua/float_term.lua
+++ float_term/lua/float_term.lua
@@ -1,41 +1,56 @@
 local M = {}
 
-function LaunchFloatingWindow(buf, opts)
-  if not vim.api.nvim_buf_is_valid(buf) then
-    vim.notify(
-    "Creating Buffer #" .. tostring(buf),
-    vim.log.levels.INFO
-  )
+local float = { win = -1, buf = -1 }
+
+function LaunchFloatingWindow(opts)
+
+  if not vim.api.nvim_buf_is_valid(float.buf) then
+    float.buf = vim.api.nvim_create_buf(false, true)
+    if float.buf == 0 then
+      error("Could not create a buffer for floating window!")
+      return
+    end
+    print("Buffer " .. float.buf .. " was created successfully")
   else
-    vim.notify(
-      "Buffer #" .. tostring(buf) .. " was found",
-      vim.log.levels.INFO
-    )
+    print("Buffer " .. float.buf .. " already exists")
   end
-  -- if not vim.api.nvim_buf_is_valid(buf) then
-  --   buf = vim.api.nvim_create_buf(false, true)
-  -- else
-  --   vim.notify("Buffer #" .. tostring(buf) .. " was found",
-  --   vim.log.levels.INFO)
-  -- end
-  -- return buf
+
+  -- Create window using nvim_open_win(buf, true, {opts})
+  if not vim.api.nvim_win_is_valid(float.win) then
+    local win_id = vim.api.nvim_open_win(float.buf, true, opts)
+    if win_id == 0 then
+      error("Could not create a new window!")
+      return
+    else
+      vim.notify("Created new window #" .. win_id, vim.log.levels.INFO)
+    end
+  else
+    float.win = win_id
+    print("Window ID #" .. float.win .. " is a valid window")
+  end
+
 end
-
-
-
-
-
 
 
 function M.setup(opts)
-  local buf = vim.api.nvim_create_buf(false, true)
+
+  local config = vim.tbl_extend("keep", opts or {}, {
+   relative = "editor", width = 60, height = 25, col = 5, row = 5
+  })
+
 
   -- setup the keymappings
   vim.keymap.set("n", "<Leader>t", function()
-    LaunchFloatingWindow(buf, opts)
+    LaunchFloatingWindow(config)
+    print("Current Mode: " .. vim.fn.mode())
   end,
   { noremap = true })
-end
 
+  vim.keymap.set("t", "<Leader>t", function()
+    vim.api.nvim_win_hide(float.win)
+  end,
+  { noremap = true })
+
+end
 
 return M
